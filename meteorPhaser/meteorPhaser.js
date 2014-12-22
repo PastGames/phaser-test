b5cf7b5d6db8a430ddf7167875c4cfdc9ded9d3b
mcoenca <m.coenca@gmail.com>
2014-12-22 09:45:35 +0100

    launch of the phaser io window inside the app thanks to the guidelines of README

--- meteorPhaser/meteorPhaser.js
+++ meteorPhaser/meteorPhaser.js
@@ -2,16 +2,11 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.game.helpers({
+    game: function () {
+      var game = new Phaser.Game(800, 600, Phaser.AUTO, "game");
+      console.log(game);
+      return;
     }
   });
 }
