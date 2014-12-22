4f301d52337c470f8f302989bd430bb75d7276c4
mcoenca <m.coenca@gmail.com>
2014-12-22 10:09:06 +0100

    phaser loading and showing a first sprite

--- meteorPhaser/meteorPhaser.js
+++ meteorPhaser/meteorPhaser.js
@@ -1,10 +1,29 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+
+  var preload = function () {
+    game.load.image('star','images/star.png');
+  };
+
+  var create = function () {
+    game.add.sprite(0, 0, 'star');
+  };
+
+  var update = function () {
+
+  };
 
   Template.game.helpers({
     game: function () {
-      var game = new Phaser.Game(800, 600, Phaser.AUTO, "game");
+      game = new Phaser.Game(
+        800,
+        600,
+        Phaser.AUTO,
+        "game",
+        {
+          preload: preload,
+          create: create,
+          update: update
+        });
       console.log(game);
       return;
     }
