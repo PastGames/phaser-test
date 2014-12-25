9874b28bfd6bea1529617138bca65de51676eec1
mcoenca <m.coenca@gmail.com>
2014-12-25 10:44:59 +0100

    first monster added

--- meteorPhaser/meteorPhaser.js
+++ meteorPhaser/meteorPhaser.js
@@ -5,6 +5,8 @@ if (Meteor.isClient) {
     game.load.image('star','images/star.png');
 
     game.load.image('foret','sprites/foret.jpg');
+
+    game.load.image('magician','sprites/monstre1.png');
   };
 
   var create = function () {
@@ -17,6 +19,16 @@ if (Meteor.isClient) {
     var background = game.add.sprite(0, 0, 'foret');
     background.scale.x = 0.5;
     background.scale.y = 0.4;
+
+    //We add a first monster to it
+    var monsters = game.add.group();
+
+    //Enable physics for everything in this group
+    monsters.enableBody = true;
+
+    //we add our first monster
+    var magician = monsters.create(100, 100, 'magician');
+    magician.scale.setTo(0.6, 0.6);
   };
 
   var update = function () {
