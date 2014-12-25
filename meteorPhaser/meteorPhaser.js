1a074b55b2408aaaab5e3b198ffce47ea023bf99
mcoenca <m.coenca@gmail.com>
2014-12-25 10:29:28 +0100

    loading and scaling the forest background image, adding some comments

--- meteorPhaser/meteorPhaser.js
+++ meteorPhaser/meteorPhaser.js
@@ -1,11 +1,22 @@
 if (Meteor.isClient) {
 
   var preload = function () {
+    //Here we load from the public folder the assets the game will need
     game.load.image('star','images/star.png');
+
+    game.load.image('foret','sprites/foret.jpg');
   };
 
   var create = function () {
-    game.add.sprite(0, 0, 'star');
+
+    //Add the physics to the game
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //How to add a simple sprite to the corner of the world, as a background
+    //and scale it
+    var background = game.add.sprite(0, 0, 'foret');
+    background.scale.x = 0.5;
+    background.scale.y = 0.4;
   };
 
   var update = function () {
